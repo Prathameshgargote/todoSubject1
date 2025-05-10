b624d062103f693224642fa1aec5fd651d5defe1
Prathameshgargote <patyagargote@gmail.com>
2025-05-10 23:23:32 +0530

    frist

--- src/app/shared/components/todolist/todolist.component.ts
+++ src/app/shared/components/todolist/todolist.component.ts
@@ -4,11 +4,42 @@ import { TodoService } from '../../services/todo.service';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { GetconfirmComponent } from '../getconfirm/getconfirm.component';
 import { SnackbarService } from '../../services/snackbar.service';
+import {
+  trigger,
+  transition,
+  query,
+  style,
+  stagger,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
   styleUrls: ['./todolist.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(-10px)' }),
+            stagger(100, [
+              animate(
+                '300ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+
+        query(':leave', [animate('200ms', style({ opacity: 0 }))], {
+          optional: true,
+        }),
+      ]),
+    ]),
+  ],
 })
 export class TodolistComponent implements OnInit {
   todoArr: Array<Itodo> = [];
@@ -21,8 +52,8 @@ export class TodolistComponent implements OnInit {
   ngOnInit(): void {
     this.fetchalltodo();
     this.updatetodo();
-    this.removetodo()
-   
+    this.removetodo();
+
     console.log(this.todoArr);
   }
 
@@ -45,14 +76,12 @@ export class TodolistComponent implements OnInit {
     });
   }
 
-
-removetodo(){
- this._todoservice.RemovetodoSub$.subscribe((res) => {
+  removetodo() {
+    this._todoservice.RemovetodoSub$.subscribe((res) => {
       let getindex = this.todoArr.findIndex((todo) => todo.todoId === res);
       this.todoArr.splice(getindex, 1);
     });
-}
-
+  }
 
   onremove(todo: Itodo) {
     let matdailogConfig = new MatDialogConfig();
